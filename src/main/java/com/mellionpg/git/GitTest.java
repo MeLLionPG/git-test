c734e9cfcd87c05c4570a85e7484ab2f104d369c
MellionPG <mellionpg@outlook.com>
2022-09-23 17:06:37 +0800

    git-test 2.0

--- src/main/java/com/mellionpg/git/GitTest.java
+++ src/main/java/com/mellionpg/git/GitTest.java
@@ -3,13 +3,14 @@ package com.mellionpg.git;
 /**
  * Git测试
  * @author MellionPG
- * @version 1.0
+ * @version 2.0
  * @since 1.0
  */
 public class GitTest {
 
     public static void main(String[] args) {
         System.out.println("Hi, Git!");
+        System.out.println("Hi, Git -version 2.0 add");
     }
 
 }
