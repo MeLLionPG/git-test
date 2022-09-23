97205911ff911abfd6ffe292fd993c37824eb9fc
MellionPG <mellionpg@outlook.com>
2022-09-23 17:02:02 +0800

    git-test 1.0

---
+++ src/main/java/com/mellionpg/git/GitTest.java
@@ -0,0 +1,15 @@
+package com.mellionpg.git;
+
+/**
+ * Git测试
+ * @author MellionPG
+ * @version 1.0
+ * @since 1.0
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hi, Git!");
+    }
+
+}
