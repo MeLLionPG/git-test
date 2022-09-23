5341985750bbcbe9444b14f2eedd3ecf22fc0c48
MellionPG <mellionpg@outlook.com>
2022-09-23 17:16:21 +0800

    hot-fix 1.0

--- src/main/java/com/mellionpg/git/GitTest.java
+++ src/main/java/com/mellionpg/git/GitTest.java
@@ -11,6 +11,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hi, Git!");
         System.out.println("Hi, Git -version 2.0 add");
+        System.out.println("hot-fix add");
     }
 
 }
